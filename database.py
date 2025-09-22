d9b7aa72d76699ae4eb4053cb67c06efc4362f8d
mrelokusa <mrelokusa@gmail.com>
2025-09-22 19:39:02 +0200

    Update database.py

--- database.py
+++ database.py
@@ -3,18 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import settings
 
-SQLALCHEMY_DATABASE_URL = settings.database_url
+# Corrected line: Use uppercase to match the variable in config.py
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-# Dependency to get a DB session
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
